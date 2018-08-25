3c55e0027c00be764d4b041be29ec4e8e3c9a075
Chan Lee <chanethanlee@gmail.com>
2018-08-25 13:05:45 -0700

    add menu-button and battle-button submit event listeners.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// Front-end ///////////////////////////////////////
+
+$(document).ready(funciton(){
+
+  $("#menu-button").submit(function(event) {
+    event.preventDefault();
+    var menuInputValue = $("#menu-input").val();
+    console.log(menutInputValue);
+    // TurnGame.getInstance().menuInput(menuInputValue);
+    $("#menu-input").val("");
+  });
+
+  $("#battle-button").submit(function(event) {
+    event.preventDefault();
+    var userBattleInput = $("battle-input").val();
+    $("battle-input").val("");
+    //TurnGame.getInstance().battleInput(userBattleInput);
+  });
+});
